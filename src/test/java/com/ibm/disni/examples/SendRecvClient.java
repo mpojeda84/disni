cfee66e9cc14c7715d37976ebc7cf8d55b54b7ad
Maikel Pereira <mpereira@maprtech.com>
2020-01-14 11:10:53 -0500

    separating class

--- src/test/java/com/ibm/disni/examples/SendRecvClient.java
+++ src/test/java/com/ibm/disni/examples/SendRecvClient.java
@@ -201,7 +201,7 @@ public class SendRecvClient implements RdmaEndpointFactory<SendRecvClient.Custom
 			wrList_recv.add(recvWR);
 
 			System.out.println("SimpleClient::initiated recv");
-			this.postRecv(wrList_recv).execute().free();
+			//this.postRecv(wrList_recv).execute().free();
 		}
 
 		public void dispatchCqEvent(IbvWC wc) throws IOException {
